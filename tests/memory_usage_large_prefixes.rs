c25f4834a4559cd3d2974a25f658d228d6c4058c
Declan Kelly <dkelly.home@gmail.com>
2022-04-03 01:22:23 -0700

    Add memory test for key distribution with large prefixes
    This new memory tests expands a fixed length key into a larger size
    with guaranteed large runs of prefixes, testing the prefix compression
    logic.
    
    This test was supposed to be roughly comparable with the fixed length
    memory test. The fixed length one used 306 keys, while the large prefix
    test used 294 keys. The fixed length keys were of length 3, the large
    prefix keys were of length 6.
    
    The large prefix test outcome had a smaller `max_blocks` value, possibly
    because the keys were concentrated into fewer inner nodes due to prefix
    compression. The `max_bytes` value of the large prefix test was 4.5
    times larger than the fixed length key. I think the cause of this is due
    (1) because the key was 2x the size, and (2) because the key is
    duplicated between the leaf nodes and the inner nodes + prefixes. These
    explanations are mostly hypothesis.

---
+++ tests/memory_usage_large_prefixes.rs
@@ -0,0 +1,75 @@
+#![feature(once_cell)]
+
+mod common;
+
+use blart::{deallocate_tree, insert_unchecked, search_unchecked, tests_common, LeafNode, NodePtr};
+use common::{get_profiler, test_heap};
+
+#[test]
+#[cfg(not(miri))]
+fn test_memory_usage() {
+    use blart::tests_common::PrefixExpansion;
+
+    const KEY_LEVEL_WIDTH: [u8; 3] = [6, 6, 5];
+    const PREFIX_EXPANSIONS: [PrefixExpansion; 2] = [
+        PrefixExpansion {
+            base_index: 0,
+            expanded_length: 3,
+        },
+        PrefixExpansion {
+            base_index: 2,
+            expanded_length: 2,
+        },
+    ];
+
+    let keys: Vec<_> =
+        tests_common::generate_key_with_prefix(KEY_LEVEL_WIDTH, PREFIX_EXPANSIONS).collect();
+    let prof = get_profiler(file!());
+
+    test_heap(&prof, |stats| {
+        dhat::assert_eq!(stats.total_blocks, 0);
+        dhat::assert_eq!(stats.total_bytes, 0);
+    });
+
+    {
+        let mut keys = keys.into_iter();
+        let mut current_root =
+            NodePtr::allocate_node(LeafNode::new(keys.next().unwrap(), 0)).to_opaque();
+
+        for (idx, key) in keys.enumerate() {
+            current_root = unsafe { insert_unchecked(current_root, key, idx + 1).unwrap() };
+        }
+
+        for (value, key) in
+            tests_common::generate_key_with_prefix(KEY_LEVEL_WIDTH, PREFIX_EXPANSIONS).enumerate()
+        {
+            let search_result = unsafe { search_unchecked(current_root, key.as_ref()) };
+
+            assert_eq!(search_result.unwrap().read().value, value);
+        }
+
+        unsafe { deallocate_tree(current_root) };
+    }
+
+    test_heap(&prof, |stats| {
+        dhat::assert_eq!(stats.curr_blocks, 0);
+        dhat::assert_eq!(stats.curr_bytes, 0);
+
+        dhat::assert_eq!(stats.max_blocks, 351);
+        dhat::assert_eq!(stats.max_bytes, 126264);
+
+        let num_keys = KEY_LEVEL_WIDTH
+            .iter()
+            .map(|val| (*val as usize) + 1)
+            .product::<usize>() as f64;
+
+        let mean_blocks_per_key = (stats.max_blocks as f64) / num_keys;
+        let mean_bytes_per_key = (stats.max_bytes as f64) / num_keys;
+
+        eprintln!(
+            "Inserting {} keys, this comes to [{} mean blockers per key] and [{} mean bytes per \
+             key].",
+            num_keys, mean_blocks_per_key, mean_bytes_per_key
+        )
+    });
+}
