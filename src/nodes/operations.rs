3946c8481d4b3b3e8bd7c8772f40d3b8b1b0409e
Declan Kelly <dkelly.home@gmail.com>
2022-04-21 22:17:38 -0700

    Factor out common deallocation code

--- src/nodes/operations.rs
+++ src/nodes/operations.rs
@@ -1,8 +1,7 @@
 //! Trie node lookup and manipulation
 
 use crate::{
-    ConcreteNodePtr, Header, InnerNode, InnerNode256Iter, InnerNode4, InnerNode48Iter,
-    InnerNodeCompressedIter, InnerNodeIter, LeafNode, NodePtr, OpaqueNodePtr,
+    ConcreteNodePtr, Header, InnerNode, InnerNode4, InnerNodeIter, LeafNode, NodePtr, OpaqueNodePtr,
 };
 use std::{collections::VecDeque, error::Error, fmt::Display, iter};
 
@@ -446,84 +445,38 @@ impl Error for InsertError {}
 ///  - This function must only be called once for this root node and all
 ///    descendants, otherwise a double-free could result.
 pub unsafe fn deallocate_tree<V>(root: OpaqueNodePtr<V>) {
+    fn deallocate_inner_node<V, N: InnerNode<Value = V>>(
+        stack: &mut Vec<OpaqueNodePtr<V>>,
+        inner_ptr: NodePtr<N>,
+    ) {
+        {
+            // SAFETY: The scope of this reference is bounded and we enforce that no
+            // mutation of the reference memory takes place within the lifetime. The
+            // deallocation of the node happens outside of this block, after the lifetime
+            // ends.
+            let inner_node = unsafe { inner_ptr.as_ref() };
+            // SAFETY: This iterator only lives for this block, a subset of the shared
+            // lifetime of the `inner_node` variable. By the safety requirements of the
+            // `deallocate_tree` function, no other mutation of this node can happen while
+            // this iterator is live.
+            let iter = unsafe { inner_node.into_iter() };
+            stack.extend(iter.map(|(_, child)| child));
+        }
+        // SAFETY: The single call per node requirement is enforced by the safety
+        // requirements on this function.
+        unsafe { NodePtr::deallocate_node(inner_ptr) }
+    }
+
     let mut stack = Vec::new();
 
     stack.push(root);
 
     while let Some(next_node_ptr) = stack.pop() {
         match next_node_ptr.to_node_ptr() {
-            ConcreteNodePtr::Node4(inner_ptr) => {
-                {
-                    // SAFETY: The scope of this reference is bounded and we enforce that no
-                    // mutation of the reference memory takes place within the lifetime. The
-                    // deallocation of the node happens outside of this block, after the lifetime
-                    // ends.
-                    let inner_node = unsafe { inner_ptr.as_ref() };
-                    // SAFETY: This iterator only lives for this block, a subset of the shared
-                    // lifetime of the `inner_node` variable. By the safety requirements of the
-                    // `deallocate_tree` function, no other mutation of this node can happen while
-                    // this iterator is live.
-                    let iter = unsafe { InnerNodeCompressedIter::new(inner_node) };
-                    stack.extend(iter.map(|(_, child)| child));
-                }
-                // SAFETY: The single call per node requirement is enforced by the safety
-                // requirements on this function.
-                unsafe { NodePtr::deallocate_node(inner_ptr) }
-            },
-            ConcreteNodePtr::Node16(inner_ptr) => {
-                {
-                    // SAFETY: The scope of this reference is bounded and we enforce that no
-                    // mutation of the reference memory takes place within the lifetime. The
-                    // deallocation of the node happens outside of this block, after the lifetime
-                    // ends.
-                    let inner_node = unsafe { inner_ptr.as_ref() };
-                    // SAFETY: This iterator only lives for this block, a subset of the shared
-                    // lifetime of the `inner_node` variable. By the safety requirements of the
-                    // `deallocate_tree` function, no other mutation of this node can happen while
-                    // this iterator is live.
-                    let iter = unsafe { InnerNodeCompressedIter::new(inner_node) };
-                    stack.extend(iter.map(|(_, child)| child));
-                }
-                // SAFETY: The single call per node requirement is enforced by the safety
-                // requirements on this function.
-                unsafe { NodePtr::deallocate_node(inner_ptr) }
-            },
-            ConcreteNodePtr::Node48(inner_ptr) => {
-                {
-                    // SAFETY: The scope of this reference is bounded and we enforce that no
-                    // mutation of the reference memory takes place within the lifetime. The
-                    // deallocation of the node happens outside of this block, after the lifetime
-                    // ends.
-                    let inner_node = unsafe { inner_ptr.as_ref() };
-                    // SAFETY: This iterator only lives for this block, a subset of the shared
-                    // lifetime of the `inner_node` variable. By the safety requirements of the
-                    // `deallocate_tree` function, no other mutation of this node can happen while
-                    // this iterator is live.
-                    let iter = unsafe { InnerNode48Iter::new(inner_node) };
-                    stack.extend(iter.map(|(_, child)| child));
-                }
-                // SAFETY: The single call per node requirement is enforced by the safety
-                // requirements on this function.
-                unsafe { NodePtr::deallocate_node(inner_ptr) }
-            },
-            ConcreteNodePtr::Node256(inner_ptr) => {
-                {
-                    // SAFETY: The scope of this reference is bounded and we enforce that no
-                    // mutation of the reference memory takes place within the lifetime. The
-                    // deallocation of the node happens outside of this block, after the lifetime
-                    // ends.
-                    let inner_node = unsafe { inner_ptr.as_ref() };
-                    // SAFETY: This iterator only lives for this block, a subset of the shared
-                    // lifetime of the `inner_node` variable. By the safety requirements of the
-                    // `deallocate_tree` function, no other mutation of this node can happen while
-                    // this iterator is live.
-                    let iter = unsafe { InnerNode256Iter::new(inner_node) };
-                    stack.extend(iter.map(|(_, child)| child));
-                }
-                // SAFETY: The single call per node requirement is enforced by the safety
-                // requirements on this function.
-                unsafe { NodePtr::deallocate_node(inner_ptr) }
-            },
+            ConcreteNodePtr::Node4(inner_ptr) => deallocate_inner_node(&mut stack, inner_ptr),
+            ConcreteNodePtr::Node16(inner_ptr) => deallocate_inner_node(&mut stack, inner_ptr),
+            ConcreteNodePtr::Node48(inner_ptr) => deallocate_inner_node(&mut stack, inner_ptr),
+            ConcreteNodePtr::Node256(inner_ptr) => deallocate_inner_node(&mut stack, inner_ptr),
             ConcreteNodePtr::LeafNode(inner) => {
                 // SAFETY: The single call per node requirement is enforced by the safety
                 // requirements on this function.
