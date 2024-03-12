abbe619f27410bcc750d20b4e215154d0955a10e
Gabriel <gaha204@hotmail.com>
2024-03-12 20:14:33 -0300

    improve fuzzy search

--- src/nodes/operations/fuzzy_search.rs
+++ src/nodes/operations/fuzzy_search.rs
@@ -1,30 +1,36 @@
 use std::intrinsics::assume;
 
-use crate::{AsBytes, InnerNode256, InnerNode48, InnerNodeCompressed, LeafNode, OpaqueNodePtr};
+use crate::{
+    AsBytes, HeaderNode, InnerNode, InnerNode256, InnerNode48, InnerNodeCompressed, LeafNode, OpaqueNodePtr
+};
 
-fn edit_dist(key: &[u8], c: u8, old: &[usize], new: &mut [usize]) {
+fn edit_dist(key: &[u8], c: u8, old: &[usize], new: &mut [usize], max_edit_dist: usize) -> bool {
     unsafe {
         assume(old.len() == new.len());
         assume(old.len() >= 1);
         assume(key.len() + 1 == old.len());
     }
+
     new[0] = old[0] + 1;
+    let mut keep = new[0] <= max_edit_dist;
     for i in 1..new.len() {
         // let cost = (key[i - 1] == c) as usize;
         // new[i] = (old[i - 1] + cost).min(old[i] + 1).min(new[i-1] + 1);
 
-        if unsafe { *key.get_unchecked(i - 1) } == c {
+        new[i] = if unsafe { *key.get_unchecked(i - 1) } == c {
             // new[i] = old[i - 1];
-            new[i] = unsafe { *old.get_unchecked(i - 1) };
+            unsafe { *old.get_unchecked(i - 1) }
         } else {
             // new[i] = (old[i - 1] + 1).min(old[i] + 1).min(new[i-1] + 1);
-            new[i] = unsafe {
+            unsafe {
                 (*old.get_unchecked(i - 1) + 1)
                     .min(*old.get_unchecked(i) + 1)
                     .min(*new.get_unchecked(i - 1) + 1)
-            };
-        }
+            }
+        };
+        keep |= new[i] <= max_edit_dist;
     }
+    keep
 }
 
 pub struct FuzzyNode<K, V> {
@@ -48,6 +54,26 @@ pub trait FuzzySearch<K, V> {
         results: &mut Vec<(&'a K, &'a V)>,
         max_edit_dist: usize,
     );
+
+    #[inline(always)]
+    fn fuzzy_search_prefix(
+        &self,
+        key: &[u8],
+        fuzzy_node: &mut FuzzyNode<K, V>,
+        new_row: &mut Vec<usize>,
+        max_edit_dist: usize,
+    ) -> bool
+    where
+        Self: HeaderNode,
+    {
+        let prefix = self.header().read_prefix();
+        let mut keep = true;
+        for k in prefix {
+            keep &= edit_dist(key, *k, &fuzzy_node.old_row, new_row, max_edit_dist);
+            std::mem::swap(&mut fuzzy_node.old_row, new_row);
+        }
+        keep
+    }
 }
 
 impl<K: AsBytes, V, const SIZE: usize> FuzzySearch<K, V> for InnerNodeCompressed<K, V, SIZE> {
@@ -58,19 +84,19 @@ impl<K: AsBytes, V, const SIZE: usize> FuzzySearch<K, V> for InnerNodeCompressed
         new_row: &mut Vec<usize>,
         fuzzy_nodes: &mut Vec<FuzzyNode<K, V>>,
         _: &mut Vec<(&'a K, &'a V)>,
-        _: usize,
+        max_edit_dist: usize,
     ) {
-        let prefix = self.header.read_prefix();
-        for k in prefix {
-            edit_dist(key, *k, &fuzzy_node.old_row, new_row);
-            std::mem::swap(&mut fuzzy_node.old_row, new_row);
+        if !self.fuzzy_search_prefix(key, fuzzy_node, new_row, max_edit_dist) {
+            return;
         }
+
         let (keys, nodes) = self.initialized_portion();
         for (k, node) in keys.iter().zip(nodes) {
             let mut new_row = vec![0usize; key.len() + 1];
-            edit_dist(key, *k, &fuzzy_node.old_row, &mut new_row);
-            let fuzzy_node = FuzzyNode::new(*node, new_row);
-            fuzzy_nodes.push(fuzzy_node);
+            if edit_dist(key, *k, &fuzzy_node.old_row, &mut new_row, max_edit_dist) {
+                let fuzzy_node = FuzzyNode::new(*node, new_row);
+                fuzzy_nodes.push(fuzzy_node);
+            }
         }
     }
 }
@@ -83,23 +109,29 @@ impl<K: AsBytes, V> FuzzySearch<K, V> for InnerNode48<K, V> {
         new_row: &mut Vec<usize>,
         fuzzy_nodes: &mut Vec<FuzzyNode<K, V>>,
         _: &mut Vec<(&'a K, &'a V)>,
-        _: usize,
+        max_edit_dist: usize,
     ) {
-        let prefix = self.header.read_prefix();
-        for k in prefix {
-            edit_dist(key, *k, &fuzzy_node.old_row, new_row);
-            std::mem::swap(&mut fuzzy_node.old_row, new_row);
+        if !self.fuzzy_search_prefix(key, fuzzy_node, new_row, max_edit_dist) {
+            return;
         }
+
         let child_pointers = self.initialized_child_pointers();
         for (k, index) in self.child_indices.iter().enumerate() {
             if index.is_empty() {
                 continue;
             }
             let mut new_row = vec![0usize; key.len() + 1];
-            edit_dist(key, k as u8, &fuzzy_node.old_row, &mut new_row);
-            let node = child_pointers[usize::from(*index)];
-            let fuzzy_node = FuzzyNode::new(node, new_row);
-            fuzzy_nodes.push(fuzzy_node);
+            if edit_dist(
+                key,
+                k as u8,
+                &fuzzy_node.old_row,
+                &mut new_row,
+                max_edit_dist,
+            ) {
+                let node = child_pointers[usize::from(*index)];
+                let fuzzy_node = FuzzyNode::new(node, new_row);
+                fuzzy_nodes.push(fuzzy_node);
+            }
         }
     }
 }
@@ -112,21 +144,27 @@ impl<K: AsBytes, V> FuzzySearch<K, V> for InnerNode256<K, V> {
         new_row: &mut Vec<usize>,
         fuzzy_nodes: &mut Vec<FuzzyNode<K, V>>,
         _: &mut Vec<(&'a K, &'a V)>,
-        _: usize,
+        max_edit_dist: usize,
     ) {
-        let prefix = self.header.read_prefix();
-        for k in prefix {
-            edit_dist(key, *k, &fuzzy_node.old_row, new_row);
-            std::mem::swap(&mut fuzzy_node.old_row, new_row);
+        if !self.fuzzy_search_prefix(key, fuzzy_node, new_row, max_edit_dist) {
+            return;
         }
+
         for (k, node) in self.child_pointers.iter().enumerate() {
             let Some(node) = node else {
                 continue;
             };
             let mut new_row = vec![0usize; key.len() + 1];
-            edit_dist(key, k as u8, &fuzzy_node.old_row, &mut new_row);
-            let fuzzy_node = FuzzyNode::new(*node, new_row);
-            fuzzy_nodes.push(fuzzy_node);
+            if edit_dist(
+                key,
+                k as u8,
+                &fuzzy_node.old_row,
+                &mut new_row,
+                max_edit_dist,
+            ) {
+                let fuzzy_node = FuzzyNode::new(*node, new_row);
+                fuzzy_nodes.push(fuzzy_node);
+            }
         }
     }
 }
@@ -144,7 +182,7 @@ impl<K: AsBytes, V> FuzzySearch<K, V> for LeafNode<K, V> {
         let current_len = fuzzy_node.old_row[0];
         let remaining_key = &self.key_ref().as_bytes()[current_len..];
         for k in remaining_key {
-            edit_dist(key, *k, &fuzzy_node.old_row, new_row);
+            edit_dist(key, *k, &fuzzy_node.old_row, new_row, max_edit_dist);
             std::mem::swap(&mut fuzzy_node.old_row, new_row);
         }
         let edit_dist = *fuzzy_node.old_row.last().unwrap();
