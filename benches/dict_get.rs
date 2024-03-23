8ed9453453499a1ed66c57e0a1a5e96ef2a2216a
Gabriel <gaha204@hotmail.com>
2024-03-22 23:59:13 -0300

    more benches

---
+++ benches/dict_get.rs
@@ -0,0 +1,54 @@
+use std::{ffi::CString, time::Duration};
+
+use blart::TreeMap;
+use criterion::{measurement::Measurement, Criterion};
+use rand::{rngs::StdRng, SeedableRng, seq::SliceRandom};
+
+fn bench<M: Measurement>(c: &mut Criterion<M>, prefix: &str) {
+    let mut rng = StdRng::seed_from_u64(69420);
+    let words = include_str!("dict.txt");
+    let mut bytes = 0;
+    let mut words: Vec<_> = words
+        .lines()
+        .map(|s| {
+            let s = CString::new(s).unwrap();
+            bytes += s.as_bytes_with_nul().len();
+            s
+        })
+        .collect();
+    words.dedup();
+    words.sort();
+
+    let mut searches: Vec<_> = words.choose_multiple(&mut rng, 5_000).cloned().collect();
+    searches.sort();
+
+    let mut tree: TreeMap<_, _> = words
+    .into_iter()
+    .map(|s| {
+        (s, 0usize)
+    })
+    .collect();
+
+    let mut group = c.benchmark_group(format!("{prefix}"));
+    group.throughput(criterion::Throughput::Bytes(bytes as u64));
+    group.warm_up_time(Duration::from_secs(10));
+    group.measurement_time(Duration::from_secs(30));
+    group.bench_function("get", |b| {
+        b.iter(
+            || {
+                for search in &searches {
+                    std::hint::black_box(tree.get(search));
+                }
+            }
+        );
+    });
+}
+
+blart::gen_benches!(
+    bench,
+    (cycles, perfcnt::linux::HardwareEventType::CPUCycles),
+    (
+        instructions,
+        perfcnt::linux::HardwareEventType::Instructions
+    )
+);
---
+++ benches/get_fuzzy.rs
@@ -0,0 +1,51 @@
+use std::{ffi::CString, time::Duration};
+
+use blart::TreeMap;
+use criterion::{measurement::Measurement, Criterion};
+
+fn bench<M: Measurement>(c: &mut Criterion<M>, prefix: &str) {
+    let words = include_str!("dict.txt");
+    let mut bytes = 0;
+    let mut tree: TreeMap<_, _> = words
+    .lines()
+    .map(|s| {
+        let s = CString::new(s).unwrap();
+        bytes += s.as_bytes_with_nul().len();
+        (s, 0usize)
+    })
+    .collect();
+
+    let searches = [
+        CString::new("house").unwrap(),
+        CString::new("houze").unwrap(),
+        CString::new("headphones").unwrap(),
+        CString::new("headpones").unwrap(),
+        CString::new("unavoided").unwrap(),
+        CString::new("unavoid").unwrap(),
+    ];
+
+    let costs = [0, 3, 5, 10];
+
+    for cost in costs {
+        let mut group = c.benchmark_group(format!("{prefix}/{cost}"));
+        group.throughput(criterion::Throughput::Bytes(bytes as u64));
+        group.warm_up_time(Duration::from_secs(5));
+        group.measurement_time(Duration::from_secs(10));
+        for search in &searches {
+            group.bench_function(search.to_str().unwrap(), |b| {
+                b.iter(
+                    || std::hint::black_box(tree.get_fuzzy(search, cost))
+                );
+            });
+        }
+    }
+}
+
+blart::gen_benches!(
+    bench,
+    (cycles, perfcnt::linux::HardwareEventType::CPUCycles),
+    (
+        instructions,
+        perfcnt::linux::HardwareEventType::Instructions
+    )
+);
