4518961a22fd2e8606071367b85e583516bad6f2
Declan Kelly <dkelly.home@gmail.com>
2024-06-19 16:04:34 -0700

    Re-enable dhat tests

--- tests/memory_usage_large_prefixes.rs
+++ tests/memory_usage_large_prefixes.rs
@@ -1,77 +1,68 @@
-// mod common;
+mod common;
 
-// #[test]
-// #[cfg(not(miri))]
-// fn test_memory_usage() {
-//     use blart::{
-//         deallocate_tree, insert_unchecked, search_unchecked, tests_common,
-//         tests_common::PrefixExpansion, LeafNode, NodePtr,
-//     };
-//     use common::{get_profiler, test_heap};
+#[test]
+#[cfg(not(miri))]
+fn test_memory_usage() {
+    use blart::{
+        TreeMap, tests_common, tests_common::PrefixExpansion
+    };
+    use common::{get_profiler, test_heap};
 
-//     const KEY_LEVEL_WIDTH: [u8; 3] = [6, 6, 5];
-//     const PREFIX_EXPANSIONS: [PrefixExpansion; 2] = [
-//         PrefixExpansion {
-//             base_index: 0,
-//             expanded_length: 9,
-//         },
-//         PrefixExpansion {
-//             base_index: 2,
-//             expanded_length: 2,
-//         },
-//     ];
+    const KEY_LEVEL_WIDTH: [u8; 3] = [6, 6, 5];
+    const PREFIX_EXPANSIONS: [PrefixExpansion; 2] = [
+        PrefixExpansion {
+            base_index: 0,
+            expanded_length: 9,
+        },
+        PrefixExpansion {
+            base_index: 2,
+            expanded_length: 2,
+        },
+    ];
 
-//     let keys: Vec<_> =
-//         tests_common::generate_key_with_prefix(KEY_LEVEL_WIDTH, PREFIX_EXPANSIONS).collect();
-//     let prof = get_profiler(file!());
+    let keys: Vec<_> =
+        tests_common::generate_key_with_prefix(KEY_LEVEL_WIDTH, PREFIX_EXPANSIONS).collect();
+    let prof = get_profiler(file!());
 
-//     test_heap(&prof, |stats| {
-//         dhat::assert_eq!(stats.total_blocks, 0);
-//         dhat::assert_eq!(stats.total_bytes, 0);
-//     });
+    test_heap(&prof, |stats| {
+        dhat::assert_eq!(stats.total_blocks, 0);
+        dhat::assert_eq!(stats.total_bytes, 0);
+    });
 
-//     {
-//         let mut keys = keys.into_iter();
-//         let mut current_root =
-//             NodePtr::allocate_node_ptr(LeafNode::new(keys.next().unwrap(), 0)).to_opaque();
+    {
+        let keys = keys.into_iter();
+        let mut tree = TreeMap::new();
 
-//         for (idx, key) in keys.enumerate() {
-//             current_root = unsafe {
-//                 insert_unchecked(current_root, key, idx + 1)
-//                     .unwrap()
-//                     .new_root
-//             };
-//         }
+        for (idx, key) in keys.enumerate() {
+            tree.try_insert(key, idx).unwrap();
+        }
 
-//         for (value, key) in
-//             tests_common::generate_key_with_prefix(KEY_LEVEL_WIDTH, PREFIX_EXPANSIONS).enumerate()
-//         {
-//             let search_result = unsafe { search_unchecked(current_root, &key) };
+        for (value, key) in
+            tests_common::generate_key_with_prefix(KEY_LEVEL_WIDTH, PREFIX_EXPANSIONS).enumerate()
+        {
+            let result = tree.get(&key).unwrap();
+            assert_eq!(result, &value);
+        }
+    }
 
-//             assert_eq!(search_result.unwrap().read().value_ref(), &value);
-//         }
+    test_heap(&prof, |stats| {
+        dhat::assert_eq!(stats.curr_blocks, 0);
+        dhat::assert_eq!(stats.curr_bytes, 0);
 
-//         unsafe { deallocate_tree(current_root) };
-//     }
+        dhat::assert_eq!(stats.max_blocks, 352);
+        dhat::assert_eq!(stats.max_bytes, 16696);
 
-//     test_heap(&prof, |stats| {
-//         dhat::assert_eq!(stats.curr_blocks, 0);
-//         dhat::assert_eq!(stats.curr_bytes, 0);
+        let num_keys = KEY_LEVEL_WIDTH
+            .iter()
+            .map(|val| (*val as usize) + 1)
+            .product::<usize>() as f64;
 
-//         dhat::assert_eq!(stats.max_blocks, 360);
-//         dhat::assert_eq!(stats.max_bytes, 17681);
+        let mean_blocks_per_key = (stats.max_blocks as f64) / num_keys;
+        let mean_bytes_per_key = (stats.max_bytes as f64) / num_keys;
 
-//         let num_keys = KEY_LEVEL_WIDTH
-//             .iter()
-//             .map(|val| (*val as usize) + 1)
-//             .product::<usize>() as f64;
-
-//         let mean_blocks_per_key = (stats.max_blocks as f64) / num_keys;
-//         let mean_bytes_per_key = (stats.max_bytes as f64) / num_keys;
-
-//         eprintln!(
-//             "Inserting {num_keys} keys, this comes to [{mean_blocks_per_key} mean blocks per key] \
-//              and [{mean_bytes_per_key} mean bytes per key].",
-//         )
-//     });
-// }
+        eprintln!(
+            "Inserting {num_keys} keys, this comes to [{mean_blocks_per_key} mean blocks per key] \
+             and [{mean_bytes_per_key} mean bytes per key].",
+        )
+    });
+}
