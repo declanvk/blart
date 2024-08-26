2ae1d2b82bb994911a756a80723dac0d8b0cc2f2
Declan Kelly <dkelly.home@gmail.com>
2024-08-25 22:03:22 -0700

    Update comment about issue in visitor trait

--- src/nodes/visitor.rs
+++ src/nodes/visitor.rs
@@ -65,14 +65,17 @@ impl<K, T, const PREFIX_LEN: usize, N: Node<PREFIX_LEN> + Visitable<K, T, PREFIX
     Visitable<K, T, PREFIX_LEN> for NodePtr<PREFIX_LEN, N>
 {
     fn super_visit_with<V: Visitor<K, T, PREFIX_LEN>>(&self, visitor: &mut V) -> V::Output {
-        // FIXME: This is broken in a couple ways:
-        //  1. The `read` function likely needs to be marked unsafe, since it does not
-        //     have any guarantees around concurrent reads or modification
-        //  2. The `Visitor`/`Visitable` trait methods likely all need to be marked
+        // FIXME(https://github.com/declanvk/blart/issues/34): This is broken in a couple ways:
+        //  1. The `Visitor`/`Visitable` trait methods likely all need to be marked
         //     unsafe, since they are operating on the raw nodes of the tree. The
         //     visitor implementations so far include "Safety" doc-comments requiring
         //     read-only access, but it probably should be recorded in the function
         //     signature
+        //  2. The `visit_with` functions work better when they're using a reference
+        //     pointing to the actual location of `N`, not a local copy on the stack.
+        //     For example, the DotPrinter will attempt to print node addresses by
+        //     converting the given reference into a pointer, but this only really works
+        //     if the reference points to the actual node location.
         let inner = self.read();
         inner.visit_with(visitor)
     }
