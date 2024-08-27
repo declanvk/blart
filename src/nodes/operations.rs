225c0fa2b82a85266ff0fdae1c54379ecb9e064b
Declan Kelly <dkelly.home@gmail.com>
2024-08-27 12:23:01 -0700

    Rework IntoIter implementation
    **Description**
    Fixes https://github.com/declanvk/blart/issues/19
    
    This commit modifies the `IntoIter` implementation to be multiple
    steps:
     1. Deallocate all the inner node of the trie, leaving only the
        leave nodes
     2. Iterate through the leaf nodes by linked list and deallocate the
        leaf node and return the key-value pairs.
     3. If there are any leaf nodes remaining on drop, deallocate all of
        them
    
    This commit also moves the tree dealloc operation into a separate
    module, and adds a couple variants of the dealloc operation to
    support the `IntoIter` modifications.
    
    The TreeMap fuzz tests are also updated to exercise the `.into_iter`
    implementation in a way that should cover all 3 steps listed above.
    
    **Motivation**
    This change makes the `IntoIter` more efficient, since we don't have
    to maintain a valid trie between each call to `next()`.
    
    **Testing Done**
    `./scripts/full-test.sh nightly`

--- src/nodes/operations.rs
+++ src/nodes/operations.rs
@@ -1,7 +1,5 @@
 //! Trie node lookup and manipulation
 
-use crate::{ConcreteNodePtr, InnerNode, NodePtr, OpaqueNodePtr};
-
 mod insert;
 pub(crate) use insert::*;
 
@@ -14,57 +12,5 @@ pub(crate) use lookup::*;
 mod delete;
 pub(crate) use delete::*;
 
-/// Deallocate the given node and all children of the given node.
-///
-/// This will also deallocate the leaf nodes with their value type data.
-///
-/// # Safety
-///  - This function must only be called once for this root node and all
-///    descendants, otherwise a double-free could result.
-pub unsafe fn deallocate_tree<K, V, const PREFIX_LEN: usize>(
-    root: OpaqueNodePtr<K, V, PREFIX_LEN>,
-) {
-    fn deallocate_inner_node<K, V, N, const PREFIX_LEN: usize>(
-        stack: &mut Vec<OpaqueNodePtr<K, V, PREFIX_LEN>>,
-        inner_ptr: NodePtr<PREFIX_LEN, N>,
-    ) where
-        N: InnerNode<PREFIX_LEN, Key = K, Value = V>,
-    {
-        {
-            // SAFETY: The scope of this reference is bounded and we enforce that no
-            // mutation of the reference memory takes place within the lifetime. The
-            // deallocation of the node happens outside of this block, after the lifetime
-            // ends.
-            let inner_node = unsafe { inner_ptr.as_ref() };
-
-            // SAFETY: This iterator only lives for this block, a subset of the shared
-            // lifetime of the `inner_node` variable. By the safety requirements of the
-            // `deallocate_tree` function, no other mutation of this node can happen while
-            // this iterator is live.
-            let iter = inner_node.iter();
-            stack.extend(iter.map(|(_, child)| child));
-        }
-
-        // SAFETY: The single call per node requirement is enforced by the safety
-        // requirements on this function.
-        drop(unsafe { NodePtr::deallocate_node_ptr(inner_ptr) });
-    }
-
-    let mut stack = Vec::new();
-
-    stack.push(root);
-
-    while let Some(next_node_ptr) = stack.pop() {
-        match next_node_ptr.to_node_ptr() {
-            ConcreteNodePtr::Node4(inner_ptr) => deallocate_inner_node(&mut stack, inner_ptr),
-            ConcreteNodePtr::Node16(inner_ptr) => deallocate_inner_node(&mut stack, inner_ptr),
-            ConcreteNodePtr::Node48(inner_ptr) => deallocate_inner_node(&mut stack, inner_ptr),
-            ConcreteNodePtr::Node256(inner_ptr) => deallocate_inner_node(&mut stack, inner_ptr),
-            ConcreteNodePtr::LeafNode(inner) => {
-                // SAFETY: The single call per node requirement is enforced by the safety
-                // requirements on this function.
-                drop(unsafe { NodePtr::deallocate_node_ptr(inner) })
-            },
-        }
-    }
-}
+mod deallocate;
+pub(crate) use deallocate::*;
