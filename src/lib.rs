15946d95742d9e21271d6aee2e63ac01586f26fa
Declan Kelly <dkelly.home@gmail.com>
2024-11-15 20:35:35 -0800

    Remove nightly feature flags which aren't required
    **Description**
    A couple of nightly features flags have been merged, so we can remove
    them. One flag was even returning an error saying it didn't exist?
    
    Once we bump the MSRV past the point where the same features would
    appear on stable, we can remove the corresponding functions from the
    `rust_nightly_apis` module.

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,10 @@
         impl_trait_in_assoc_type,
         maybe_uninit_slice,
         maybe_uninit_uninit_array,
-        const_maybe_uninit_uninit_array,
         maybe_uninit_array_assume_init,
         slice_ptr_get,
         hasher_prefixfree_extras,
-        new_uninit,
         core_intrinsics,
-        strict_provenance,
         portable_simd
     )
 )]
