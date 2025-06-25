6b40436bba997ffb7e4697b54ea8ed343cfdb450
Declan Kelly <dkelly.home@gmail.com>
2025-06-25 14:17:00 -0700

    Upgrade `rand` and remove deprecated `ZipfDistribution`
    **Description**
     - Upgrade `rand` to latest version
     - Add `rand_distr` dev dependency to replace `zipf::ZipfDistribution`
       since it is deprecated
    
    **Testing Done**
    `./scripts/full-test.sh nightly`

--- benches/tree/iter.rs
+++ benches/tree/iter.rs
@@ -2,7 +2,7 @@ use std::{ffi::CString, ptr::NonNull};
 
 use blart::raw::{InnerNode, InnerNode16, InnerNode256, InnerNode4, InnerNode48, NodePtr};
 use criterion::{criterion_group, measurement::Measurement, Criterion};
-use rand::{rngs::StdRng, seq::SliceRandom, SeedableRng};
+use rand::{rngs::StdRng, seq::IndexedRandom, SeedableRng};
 
 use crate::common::{dictionary_tree, get_middle_key};
 
