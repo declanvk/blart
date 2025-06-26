6a709b186f48f6cc695618e06847a513da537ece
Declan Kelly <dkelly.home@gmail.com>
2025-06-26 14:29:08 -0700

    Add script to serve coverage report

---
+++ scripts/serve-cov.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -o errexit # make script exit when a command fails
+set -o nounset # make script exit when using undeclared variables
+set -o pipefail # make script exit when command fails in a pipe
+set -o xtrace # print a trace of all commands executed by script
+
+cargo llvm-cov report --html
+
+python3 -m http.server --directory target/llvm-cov/html
