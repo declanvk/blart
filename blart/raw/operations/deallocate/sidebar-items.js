019106e5a4b28c5f4da3333d5c40d06ccc7d66f9
declanvk <dkelly.home@gmail.com>
2025-07-01 23:56:01 +0000

    Deploying to gh-pages from @ declanvk/blart@dc8161d26419cf8835b31eb3bd1497a818f085dc 🚀

---
+++ blart/raw/operations/deallocate/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":["deallocate_inner_node","deallocate_leaves","deallocate_tree","deallocate_tree_non_leaves"]};
---
+++ trait.impl/core/fmt/trait.Pointer.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["blart",[["impl&lt;K, V, const PREFIX_LEN: <a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.usize.html\">usize</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Pointer.html\" title=\"trait core::fmt::Pointer\">Pointer</a> for <a class=\"struct\" href=\"blart/raw/struct.OpaqueNodePtr.html\" title=\"struct blart::raw::OpaqueNodePtr\">OpaqueNodePtr</a>&lt;K, V, PREFIX_LEN&gt;"],["impl&lt;P, const MIN_BITS: <a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.u32.html\">u32</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Pointer.html\" title=\"trait core::fmt::Pointer\">Pointer</a> for <a class=\"struct\" href=\"blart/tagged_pointer/struct.TaggedPointer.html\" title=\"struct blart::tagged_pointer::TaggedPointer\">TaggedPointer</a>&lt;P, MIN_BITS&gt;"],["impl&lt;const PREFIX_LEN: <a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.usize.html\">usize</a>, N&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Pointer.html\" title=\"trait core::fmt::Pointer\">Pointer</a> for <a class=\"struct\" href=\"blart/raw/struct.NodePtr.html\" title=\"struct blart::raw::NodePtr\">NodePtr</a>&lt;PREFIX_LEN, N&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1291]}
